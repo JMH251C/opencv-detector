666810a088021365aef5bb343f1655b2c2810f0c
JMH251C <120239588+JMH251C@users.noreply.github.com>
2023-03-06 23:08:30 +0800

    Add files via upload

---
+++ face.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread("/home/jmh/3.jpg")
+
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+faces = faceCascade.detectMultiScale(img,1.15)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x + w,y + h),(0,0,255),5)
+
+    cv2.imshow("img",img)
+    cv2.waitKey()
